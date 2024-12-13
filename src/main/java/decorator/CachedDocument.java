99ac9ea06be7bf2986dfbddc7c9ad6f554a7ecf7
nadiikafedii <115722979+nadiikafedii@users.noreply.github.com>
2024-12-13 19:32:07 +0200

    Update CachedDocument.java

--- src/main/java/decorator/CachedDocument.java
+++ src/main/java/decorator/CachedDocument.java
@@ -3,16 +3,18 @@ package decorator;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class CachedDocument implements Document{
+public class CachedDocument implements Document {
     private Document document;
     @Override
     public String parse() {
-        String cashed = DBConnection.getInstance().getDocument(document.getGcsPath());
+        String cashed = DBConnection.getInstance().getDocument(
+            document.getGcsPath());
         if (cashed != null) {
             return cashed;
         } else {
             String parsed = document.parse();
-            DBConnection.getInstance().createDocument(document.getGcsPath(), parsed);
+            DBConnection.getInstance().createDocument(
+                document.getGcsPath(), parsed);
             return parsed;
         }
     }
